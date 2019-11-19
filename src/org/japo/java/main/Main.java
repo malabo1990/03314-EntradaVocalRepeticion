8ef7c5831a354a3dc91955955c21e73b577909a8
malabo1990 <malabo1990@gmail.com>
2019-11-19 17:06:26 +0100

    Add files via upload

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,47 @@
+/* 
+ * Copyright 2019 User.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Casimiro Ondo Obiang
+ */
+public class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+        boolean OK = false;
+        char vocal;
+        do {
+            try {
+                System.out.print("Introduzca una vocal mayuscula: ");
+                vocal = SCN.nextLine().charAt(0);
+                char vocalUpper = Character.toUpperCase(vocal);
+                System.out.printf("Vocal seleccionada ...: %c%n", vocalUpper);
+                OK = true;
+            } catch (Exception e) {
+                System.out.println("ERROR: Dato incorrecto");
+            }
+        } while (!OK);
+    }
+}
